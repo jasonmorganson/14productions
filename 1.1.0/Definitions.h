83910dd3a342cc3e044a546ae9fab4037f8bcc68
jmorganson <jmorganson@c81cd575-1920-0410-b3dd-e12d79d8fed8>
2006-10-19 02:28:23 +0000

    made a copy

---
+++ 1.1.0/Definitions.h
@@ -0,0 +1,16 @@
+/* A list of definitions */
+
+#ifndef _DEFINITIONS_H_
+#define _DEFINITIONS_H_
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define DEBUG true  // Toggles extra information that could be useful for debugging.
+#define debug(s) (cout << s << endl)  // Macro for displaying debugging information.
+
+typedef HWND WINDOW_TYPE;  // Defines the window type to be used. HWND for Windows or Window for Linux.
+
+#endif
